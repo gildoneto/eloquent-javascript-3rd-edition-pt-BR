f08833d29881efedab289945ff6f4f07c30663c8
Gildo Neto <gildorama@gmail.com>
2021-06-02 17:37:52 -0300

    feat: adiciona pasta img

---
+++ eloquent-javascript-3rd-edition/code.js
@@ -0,0 +1,8 @@
+// 01 - LOOPING A TRIANGLE - Eloquent JavaScript
+// the code below was written by @gildoneto - 01/06/2021
+
+let secret = "";
+while (secret.length < 7){
+    secret += "#";
+    console.log(secret);
+}
