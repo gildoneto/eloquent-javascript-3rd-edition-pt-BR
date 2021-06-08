e21b06f9efff986dd007cd232d927a265d902482
Gildo Neto <gildorama@gmail.com>
2021-06-08 11:56:12 -0300

    refactor: atualiza

--- eloquent-javascript-3rd-edition/code.js
+++ eloquent-javascript-3rd-edition/code.js
@@ -6,3 +6,25 @@ while (secret.length < 7){
     secret += "#";
     console.log(secret);
 }
+
+/* ######################################################## */
+
+// 02 - FizzBuzz - Eloquent JavaScript
+// the code below was written by @gildoneto - 01/06/2021
+
+let number = 1;
+while (number <= 100){
+	if (number % 3 == 0 && number % 5 == 0){
+        console.log(number + "FizzBuzz");
+    }
+  	else if (number % 3 == 0){
+      console.log(number + "Fizz");
+	}
+  	else if (number % 5 == 0){
+      console.log(number + "Buzz");
+	}
+  	else {
+    console.log(number);
+    }
+	number += 1;
+}
