118d252e107158e6c8151e03143b191c89449ffd
Gildo Neto <gildorama@gmail.com>
2021-06-10 19:31:09 -0300

    refactor: adiciona chessboard

--- eloquent-javascript-3rd-edition/code.js
+++ eloquent-javascript-3rd-edition/code.js
@@ -28,3 +28,75 @@ while (number <= 100){
     }
 	number += 1;
 }
+
+/* ######################################################## */
+
+// 03 - CHESSBOARD - Eloquent JavaScript
+// the code below was written by @gildoneto - 01/06/2021
+
+let space = " ", hash= "#", line = "\n";
+let tam = 8;
+let xadrez = "";
+let linhaPar = "";
+let linhaImpar = "";
+    for (let i = 0; i < tam; i++){
+      if (i % 2 == 0){
+          linhaPar = linhaPar + space;
+          linhaImpar = linhaImpar + hash;
+      }
+      else {
+          linhaPar = linhaPar + hash;
+          linhaImpar = linhaImpar + space;
+      }
+    }
+	
+	for (let i = 0; i < tam; i++){
+    if (i % 2 == 1){
+          xadrez = xadrez + linhaPar + line;
+      }
+      else {
+      		xadrez = xadrez + linhaImpar + line;
+      }
+    }
+console.log(xadrez);
+
+/* ######################################################## */
+
+// 04 - THE SUM OF A RANGE - Eloquent JavaScript
+// the code below was written by @gildoneto - ##/06/2021
+
+
+/* ######################################################## */
+
+// 05 - REVERSING AN ARRAY - Eloquent JavaScript
+// the code below was written by @gildoneto - ##/06/2021
+
+
+/* ######################################################## */
+
+// 06 - A LIST - Eloquent JavaScript
+// the code below was written by @gildoneto - ##/06/2021
+
+
+/* ######################################################## */
+
+// 07 - DEEP COMPARISON - Eloquent JavaScript
+// the code below was written by @gildoneto - ##/06/2021
+
+
+/* ######################################################## */
+
+// 08 - ####### - Eloquent JavaScript
+// the code below was written by @gildoneto - ##/06/2021
+
+
+/* ######################################################## */
+
+// 09 - ####### - Eloquent JavaScript
+// the code below was written by @gildoneto - ##/06/2021
+
+
+/* ######################################################## */
+
+// 10 - ####### - Eloquent JavaScript
+// the code below was written by @gildoneto - ##/06/2021
